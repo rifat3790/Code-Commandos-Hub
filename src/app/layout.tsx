c9f790ae1d69f6c96d82cddfcfd6658b89c2bc69
jcab118 <jcab118@gmail.com>
2026-07-12 16:51:14 +0600

    force bypass extension bis_skin_checked hydration mismatch by strip script in head layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,41 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const strip = (el) => {
+                  if (el && el.nodeType === 1) {
+                    if (el.hasAttribute('bis_skin_checked')) {
+                      el.removeAttribute('bis_skin_checked');
+                    }
+                    const children = el.getElementsByTagName('*');
+                    for (let i = 0; i < children.length; i++) {
+                      if (children[i].hasAttribute('bis_skin_checked')) {
+                        children[i].removeAttribute('bis_skin_checked');
+                      }
+                    }
+                  }
+                };
+                const observer = new MutationObserver((mutations) => {
+                  for (let i = 0; i < mutations.length; i++) {
+                    const added = mutations[i].addedNodes;
+                    for (let j = 0; j < added.length; j++) {
+                      strip(added[j]);
+                    }
+                  }
+                });
+                observer.observe(document.documentElement, {
+                  childList: true,
+                  subtree: true
+                });
+              })();
+            `
+          }}
+        />
+      </head>
       <body className="h-full bg-[#030712] text-[#f3f4f6] antialiased overflow-hidden" suppressHydrationWarning>
         <ClientLayout>
           {children}
